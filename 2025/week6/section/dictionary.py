7b449e45ae1148f4adbc8740d49f9ac55eced3bb
vibhmitra <66880260+vibhmitra@users.noreply.github.com>
2025-10-03 00:07:52 +0530

    Create dictionary.py

---
+++ 2025/week6/section/dictionary.py
@@ -0,0 +1,6 @@
+songs =
+[{ "name": "Perfect", "tempo": 95.05, "album":"Divide" },
+ { "name": "Eastside", "tempo": 89.391, "album":"Why" },
+ { "name": "Runaway", "tempo": 95.05, "album":"IDK" }]
+
+print(songs[2]["name"])
