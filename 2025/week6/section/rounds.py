e24d1f4ee1cab6d588cdf6c30e3eeee9a27a9e5e
vibhmitra <66880260+vibhmitra@users.noreply.github.com>
2025-10-03 00:06:22 +0530

    Update rounds.py

--- 2025/week6/section/rounds.py
+++ 2025/week6/section/rounds.py
@@ -1,8 +1,63 @@
-for i in range(3):
-    print(i)
+# round 1
+# phrase = "You're off to Great Places"
+
+# print("Round 1: ", end="")
+
+# for i in range(0, len(phrase), 2):
+#     print(phrase[i], end="")
+# print()
+
+# O/P
+# 2025/week6/section/ $ python round1.py
+# Round 1: Yur f oGetPae
 
 
-pharase = "You're off to Great Places"
+# # round 2
+# phrase = "You're off to Great Places"
 
-for char in range(len(phrase)):
-    print(char)
+# print("Round 2: ", end="")
+
+# for i in range(1, len(phrase) - 1):
+#     print(phrase[i], end="")
+# print("")
+
+# # O/P
+# # 2025/week6/section/ $ python round1.py
+# # Round 2: ou're off to Great Place
+
+# round 3
+# phrase = "You're off to Great Places"
+
+# print("Round 3: ", end="")
+
+# for chars in phrase:
+#     print(chars, end=" ")
+# print("")
+
+# # O/P
+# # 2025/week6/section/ $ python round1.py
+# # Round 3: Y o u ' r e   o f f   t o   G r e a t   P l a c e s
+
+# round 4
+
+# phrase = "You're off to Great Places"
+
+# print("Round 4: ", end="")
+
+# for chars in phrase[7:]:
+#     print(chars, end=" ")
+# print("")
+
+# # O/P
+# # 2025/week6/section/ $ python round1.py
+# # Round 4: o f f   t o   G r e a t   P l a c e
+
+# round 5
+
+# phrase = "You're off to Great Places"
+
+# print("Round 5: ", end="")
+
+# for word in phrase.split(" "):
+#     print(word)
+# print("")
